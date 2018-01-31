d7f6772963e8cb81927d71b21463f52a0d0aa8c0
Timo Tyrrell <timomitchel23@gmail.com>
2018-01-31 16:43:22 -0700

    first feature test written and vcr/web config added to rails helper

---
+++ spec/features/user_logs_in_with_github_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+def stub_omniauth
+  OmniAuth.config.test_mode = true
+  OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new({
+    provider: "github",
+    uid: "23040094", 
+    info: {"nickname"=>"timomitchel", "email"=>"timomitchel23@gmail.com", "name"=>"Timothy Tyrrell", "urls"=>{"GitHub"=>"https://github.com/timomitchel", "Blog"=>""}},
+    credentials:{"token"=>"9950576d1c4753e5beb1e364d93a25a4a7e4c981", "expires"=>false},
+  })
+end
+
+RSpec.feature "user logs in" do
+  scenario "using github oauth2" do
+    stub_omniauth
+    visit root_path
+    expect(page).to have_link("Sign in with Github")
+    click_link "Sign in with Github"
+    expect(page).to have_content("Timo")
+    expect(page).to have_link("Logout")
+  end
+end
