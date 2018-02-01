0e7cca18603e784090eab9e8fd9071ef0e1eb5a1
Timo Tyrrell <timomitchel23@gmail.com>
2018-01-31 18:45:52 -0700

    add destroy and update sessions create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,15 @@
 class SessionsController < ApplicationController
 
   def create
+    user = User.update_or_create(request.env["omniauth.auth"])
+    session[:id] = user.id
+    redirect_to root_path
+  end
 
+  def destroy
+    # cookies.delete current_user.uid
+    session.clear
+    redirect_to root_path
   end
 
 end
