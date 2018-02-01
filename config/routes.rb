607271cadfa4711676c199b3658e80b7326852c2
Timo Tyrrell <timomitchel23@gmail.com>
2018-01-31 18:46:48 -0700

    add logout path

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: 'home#index'
   get '/auth/github', as: :github
   get "/auth/:provider/callback",  to: 'sessions#create'
+  get '/logout', to: "sessions#destroy", as: :logout
 end
